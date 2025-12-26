2745bf84d60e08f378a689c1e6c9f19ea72e4585
Techie_Steve <tuwa.1simon@gmail.com>
2025-12-26 21:10:09 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,14 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/',
+        source: '/index',
         destination: '/index.html',
         permanent: true, // or false if you prefer 302
+      },
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: true,
       },
     ];
   },
